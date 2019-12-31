884b2eed7faf09bbce3dc91e4d0235e14a29e8ce
dbdraganov <dbdraganov@gmail.com>
2019-12-31 16:31:01 +0200

    new logo

--- model/Host.php
+++ model/Host.php
@@ -14,7 +14,13 @@ class Host{
     public function connect() {
         $this->connection = mysqli_connect($this->host, $this->user, $this->password, $this->database);
         mysqli_set_charset($this->connection, $this->charset);
+        if(!$this->connection){
+            echo "Error: Unable to connect to MySQL." . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        }else{
         return $this->connection;
+        }
         
     }
     
